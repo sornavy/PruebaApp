7501dcb2b7ff1f6e892e3d389c4c3174498630ed
sornavy <76693629+sornavy@users.noreply.github.com>
2021-07-02 11:02:32 +0200

    UPDATED

---
+++ src/provaApp.java
@@ -0,0 +1,10 @@
+
+public class provaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//new line
+		System.out.print("new line");
+	}
+
+}
---
+++ src/provaApp/M.java
@@ -0,0 +1,10 @@
+package provaApp;
+
+public class M {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
